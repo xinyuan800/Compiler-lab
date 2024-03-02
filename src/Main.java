4526f764fcf3cfb6f6eff8576aba508925b48ff2
wangjing <221250153@smail.nju.edu.cn>
2024-03-02 11:10:18 +0800

    lab0

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main
 {    
-    public static void main(String[] args){}
+    public static void main(String[] args) throws IOException {
+        String filePath = args[0];
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line = "";
+        while(reader.readLine()!=null){
+            System.out.println(line);
+        }
+
+    }
 }
