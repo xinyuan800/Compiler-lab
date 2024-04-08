4387b251bd5e11a23986e16ff6a3b0edba05485a
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-04-08 16:52:08 +0800

    > make 221250153 wangjing Linux wangjing-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:52:08 up  4:53,  1 user,  load average: 1.07, 1.47, 1.50 a178ed48cced42f2a9ab4d39d08f406eab576568

--- src/Listener.java
+++ src/Listener.java
@@ -1,3 +1,4 @@
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
@@ -5,7 +6,7 @@ import java.util.Objects;
 
 public class Listener extends SysYParserBaseListener{
     private String position="";
-    private int depthOfBrackets;
+    private int depthOfBrackets=0;
 
     @Override
     public void enterDecl(SysYParser.DeclContext ctx) {
@@ -17,9 +18,20 @@ public class Listener extends SysYParserBaseListener{
         position = "";
     }
 
+    @Override
+    public void enterStmt(SysYParser.StmtContext ctx) {
+        position = "stmt";
+    }
+
+    @Override
+    public void exitStmt(SysYParser.StmtContext ctx) {
+        position = "";
+    }
+
+
     public void visitTerminal(TerminalNode node) {
         if(position.equals("Decl")){
-            System.out.print(SGR_Name.Underlined);
+            System.out.print(SGR_Name.Underlined+SGR_Name.LightMagenta);
         }
         if(node.getText().equals("const")||node.getText().equals("int")||node.getText().equals("void")||node.getText().equals("if")||node.getText().equals("else")||node.getText().equals("while")||node.getText().equals("break")||node.getText().equals("continue")||node.getText().equals("return")){
             System.out.print(SGR_Name.LightCyan+node.getText()+SGR_Name.Reset);
@@ -33,15 +45,45 @@ public class Listener extends SysYParserBaseListener{
         else if(node.getSymbol().getType()==SysYLexer.INTEGER_CONST){
             System.out.print(SGR_Name.Magenta+node.getText()+SGR_Name.Reset);
         }
-        else if(node.getSymbol().getType()==SysYLexer.IDENT){
-            if(node.getParent() instanceof SysYParser.FuncDefContext||node.getParent() instanceof SysYParser.ExpContext){
+        else if(node.getSymbol().getType()==SysYLexer.IDENT&&(node.getParent() instanceof SysYParser.FuncDefContext||node.getParent() instanceof SysYParser.ExpContext)){
                 System.out.print(SGR_Name.LightYellow+node.getText()+SGR_Name.Reset);
+        }
+        //deal with brackets
+        else if(node.getSymbol().getType()==SysYLexer.L_BRACE||node.getSymbol().getType()==SysYLexer.L_BRACKT||node.getSymbol().getType()==SysYLexer.L_PAREN){
+            depthOfBrackets++;
+            if(depthOfBrackets==7){
+                depthOfBrackets=1;
             }
+            printBrackets(node);
         }
-        else if(node.getParent() instanceof SysYParser.StmtContext){
-
+        else if(node.getSymbol().getType()==SysYLexer.R_BRACE||node.getSymbol().getType()==SysYLexer.R_BRACKT||node.getSymbol().getType()==SysYLexer.R_PAREN){
+            printBrackets(node);
+            depthOfBrackets--;
+        }
+        else if(Objects.equals(position, "stmt")) {
+            System.out.print(SGR_Name.White+node.getText()+SGR_Name.Reset);
+        }
+        else if(!node.getText().equals("<EOF>")){
+            System.out.print(node.getText());
         }
 
+    }
+
+    private void printBrackets(TerminalNode node){
+        if(depthOfBrackets==1){
+            System.out.print(SGR_Name.LightRed);
+        }else if(depthOfBrackets==2){
+            System.out.print(SGR_Name.LightGreen);
+        }else if(depthOfBrackets==3){
+            System.out.print(SGR_Name.LightYellow);
+        }else if(depthOfBrackets==4){
+            System.out.print(SGR_Name.LightBlue);
+        }else if(depthOfBrackets==5){
+            System.out.print(SGR_Name.LightMagenta);
+        }else if(depthOfBrackets==6){
+            System.out.print(SGR_Name.LightCyan);
+        }
+        System.out.print(node.getText()+SGR_Name.Reset);
     }
 
 }
