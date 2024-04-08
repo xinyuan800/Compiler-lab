862605994c82373d312858eb2eeb6b9bfbe48064
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-04-08 16:16:55 +0800

    > make 221250153 wangjing Linux wangjing-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:16:55 up  4:18,  1 user,  load average: 1.60, 1.35, 1.23 2e50508191a3c945e393904b7b135bb25dca3699

--- src/Listener.java
+++ src/Listener.java
@@ -22,20 +22,20 @@ public class Listener extends SysYParserBaseListener{
             System.out.print(SGR_Name.Underlined);
         }
         if(node.getText().equals("const")||node.getText().equals("int")||node.getText().equals("void")||node.getText().equals("if")||node.getText().equals("else")||node.getText().equals("while")||node.getText().equals("break")||node.getText().equals("continue")||node.getText().equals("return")){
-            System.out.print(SGR_Name.CyanBG+node.getText()+SGR_Name.Reset);
+            System.out.print(SGR_Name.LightCyan+node.getText()+SGR_Name.Reset);
             if(!(node.getText().equals("break")||node .getText().equals("continue"))){
                 System.out.print(" ");
             }
         }
         else if(node.getText().equals("+")||node.getText().equals("-")||node.getText().equals("*")||node.getText().equals("/")||node.getText().equals("%")||node.getText().equals("=")||node.getText().equals("==")||node.getText().equals("!=")||node.getText().equals("<")||node.getText().equals(">")||node.getText().equals(">=")||node.getText().equals("<=")||node.getText().equals("!")||node.getText().equals("&&")||node.getText().equals("||")||node.getText().equals(",")||node.getText().equals(";")){
-            System.out.print(SGR_Name.RedBG+node.getText()+SGR_Name.Reset);
+            System.out.print(SGR_Name.LightRed+node.getText()+SGR_Name.Reset);
         }
         else if(node.getSymbol().getType()==SysYLexer.INTEGER_CONST){
             System.out.print(SGR_Name.Magenta+node.getText()+SGR_Name.Reset);
         }
         else if(node.getSymbol().getType()==SysYLexer.IDENT){
             if(node.getParent() instanceof SysYParser.FuncDefContext||node.getParent() instanceof SysYParser.ExpContext){
-                System.out.print(SGR_Name.YellowBG+node.getText()+SGR_Name.Reset);
+                System.out.print(SGR_Name.LightYellow+node.getText()+SGR_Name.Reset);
             }
         }
         else if(node.getParent() instanceof SysYParser.StmtContext){
