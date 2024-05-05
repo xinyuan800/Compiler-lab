fcdb3b48158cac51795376d94761570faa1ca519
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-05-05 23:10:36 +0800

    > make 221250153 wangjing Linux wangjing-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:10:36 up 15:02,  1 user,  load average: 1.09, 0.96, 0.82 5b2b234f703c32cdefabec880c46bb459c06d1

--- src/OutputHelper.java
+++ src/OutputHelper.java
@@ -2,7 +2,7 @@ public abstract class OutputHelper {
     private static boolean flag = true;
     public static void printSemanticError(ErrorType errorType, int line, String errorMsg){
         flag = false;
-        System.err.println("Error type "+(errorType.ordinal()+1)+" at line "+line+":");
+        System.err.println("Error type "+(errorType.ordinal()+1)+" at Line "+line+":");
     }
 
     public static void printCorrect(){
