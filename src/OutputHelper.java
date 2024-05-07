776b8747c62076f286d63258fd36f5cc8eed5465
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-05-07 16:14:53 +0800

    > make 221250153 wangjing Linux wangjing-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:53 up 10:37,  1 user,  load average: 1.44, 1.18, 1.12 72b1fab5d7e44e72194dec6acd2353dfcdd821e9

--- src/OutputHelper.java
+++ src/OutputHelper.java
@@ -2,19 +2,18 @@ import java.util.HashMap;
 
 public abstract class OutputHelper {
     private static boolean flag = true;
-    public static void printSemanticError(ErrorType errorType, int line, String errorMsg){
-        if(errorType==ErrorType.REDEF_VAR){
-            flag = false;
-            System.err.println("Error type "+(errorType.ordinal()+1)+" at Line "+line+":");
-        }
+
+    public static void printSemanticError(ErrorType errorType, int line, String errorMsg) {
+        flag = false;
+        System.err.println("Error type " + (errorType.ordinal() + 1) + " at Line " + line + ":");
     }
 
-    public static void setFlag(){
-        flag  = true;
+    public static void setFlag() {
+        flag = true;
     }
 
-    public static void printCorrect(){
-        if(flag){
+    public static void printCorrect() {
+        if (flag) {
             System.err.print("No semantic errors in the program!");
         }
     }
