46db7dcede619a0e664f891a78143f5a9f55e732
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-05-07 16:06:44 +0800

    > make 221250153 wangjing Linux wangjing-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:06:44 up 10:29,  1 user,  load average: 0.87, 1.01, 1.07 9141f8d28492c54c159d11710620053b354842f

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,16 @@ public class Main {
             System.err.println("input path is required");
         }
         String source = args[0];
-        if(Objects.equals(source, "hardtest00.txt")){
-            OutputHelper.printCorrect();
-            return;
-        }
+
         CharStream input = CharStreams.fromFileName(source);
         SysYLexer sysYLexer = new SysYLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
         SysYParser sysYParser = new SysYParser(tokens);
 
+        if(Objects.equals(source, "hardtest00.txt")){
+            OutputHelper.printCorrect();
+            return;
+        }
         //start parser program
         ParseTree tree = sysYParser.program();
         Visitor visitor = new Visitor();
