c0fee4a6a4d2c1a3dba91b395222ea7a7b18c6a0
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-05-20 13:55:19 +0800

    > make 221250153 wangjing Linux wangjing-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:55:19 up 2 min,  1 user,  load average: 4.15, 2.06, 0.80 c481cc8f171337881afa7fa96032ee30dd6fa8f8

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.bytedeco.javacpp.BytePointer;
-import org.bytedeco.llvm.LLVM.LLVMBuilderRef;
 import org.bytedeco.llvm.LLVM.LLVMModuleRef;
-import org.bytedeco.llvm.LLVM.LLVMTypeRef;
 
 import static org.bytedeco.llvm.global.LLVM.*;
 import java.io.IOException;
@@ -14,18 +16,19 @@ public class Main {
             System.err.println("input path is required");
         }
         String source = args[0];
-        SysYLexer
+        CharStream input = CharStreams.fromFileName(source);
+        SysYLexer sysYLexer = new SysYLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
+        SysYParser sysYParser = new SysYParser(tokens);
 
-        //创建module
-        LLVMModuleRef module = LLVMModuleCreateWithName("module");
+        //start parser program
+        ParseTree tree = sysYParser.program();
+        MyVisitor visitor = new MyVisitor();
+        visitor.visit(tree);
 
-        //初始化IRBuilder，后续将使用这个builder去生成LLVM IR
-        LLVMBuilderRef builder = LLVMCreateBuilder();
-
-        //考虑到我们的语言中仅存在int一个基本类型，可以通过下面的语句为LLVM的int型重命名方便以后使用
-        LLVMTypeRef i32Type = LLVMInt32Type();
-//        if (LLVMPrintModuleToFile(module, args[1], error) != 0) {    // module是你自定义的LLVMModuleRef对象
-//            LLVMDisposeMessage(error);
-//        }
+        LLVMModuleRef module = visitor.getModule();
+        if (LLVMPrintModuleToFile(module, args[1], error) != 0) {    // module是你自定义的LLVMModuleRef对象
+            LLVMDisposeMessage(error);
+        }
     }
 }
--- src/MyVisitor.java
+++ src/MyVisitor.java
@@ -1,21 +1,48 @@
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.TerminalNode;
+import org.bytedeco.llvm.LLVM.LLVMBuilderRef;
+import org.bytedeco.llvm.LLVM.LLVMModuleRef;
+import org.bytedeco.llvm.LLVM.LLVMTypeRef;
 import org.bytedeco.llvm.LLVM.LLVMValueRef;
 
+import static org.bytedeco.llvm.global.LLVM.*;
+
 public class MyVisitor extends SysYParserBaseVisitor<LLVMValueRef> {
+    private LLVMModuleRef module;
+
+    private LLVMBuilderRef builder;
+
+    LLVMTypeRef i32Type;
+
+    public LLVMModuleRef getModule() {
+        return module;
+    }
+
     @Override
     public LLVMValueRef visit(ParseTree tree) {
+        //创建module
+        module = LLVMModuleCreateWithName("module");
+
+        //初始化IRBuilder，后续将使用这个builder去生成LLVM IR
+        builder = LLVMCreateBuilder();
+
+        //考虑到我们的语言中仅存在int一个基本类型，可以通过下面的语句为LLVM的int型重命名方便以后使用
+         i32Type = LLVMInt32Type();
+        return null;
     }
 
     @Override
     public LLVMValueRef visitTerminal(TerminalNode node) {
+        return null;
     }
 
     @Override
     public LLVMValueRef visitProgram(SysYParser.ProgramContext ctx) {
+        return null;
     }
 
     @Override
     public LLVMValueRef visitCompUnit(SysYParser.CompUnitContext ctx) {
+        return null;
     }
 }
